ad4d9c1df3c97f77fd083c9b7445263a8bc4ed2c
Allisson Azevedo <allisson@gmail.com>
2022-09-12 21:24:14 -0300

    feat: use code from github.com/allisson/sqlquery

--- go.mod
+++ go.mod
@@ -4,13 +4,14 @@ go 1.18
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/allisson/sqlquery v1.0.1
 	github.com/georgysavva/scany v1.2.0
-	github.com/huandu/go-sqlbuilder v1.15.1
 	github.com/stretchr/testify v1.8.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/huandu/go-sqlbuilder v1.15.1 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- helper.go
+++ helper.go
@@ -4,8 +4,29 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/allisson/sqlquery"
 	"github.com/georgysavva/scany/sqlscan"
 )
+
+type Flavor = sqlquery.Flavor
+type FindOptions = sqlquery.FindOptions
+type FindAllOptions = sqlquery.FindAllOptions
+
+var (
+	MySQLFlavor      = sqlquery.MySQLFlavor
+	PostgreSQLFlavor = sqlquery.PostgreSQLFlavor
+	SQLiteFlavor     = sqlquery.SQLiteFlavor
+)
+
+// NewFindOptions returns a FindOptions.
+func NewFindOptions(flavor Flavor) *FindOptions {
+	return sqlquery.NewFindOptions(flavor)
+}
+
+// NewFindAllOptions returns a FindAllOptions.
+func NewFindAllOptions(flavor Flavor) *FindAllOptions {
+	return sqlquery.NewFindAllOptions(flavor)
+}
 
 // Querier is a abstraction over *sql.DB/*sql.Conn/*sql.Tx.
 type Querier interface {
@@ -13,34 +34,34 @@ type Querier interface {
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 }
 
-// Get is a high-level function that calls FindQuery and scany sqlscan.Get function.
+// Get is a high-level function that calls sqlquery.FindQuery and scany sqlscan.Get function.
 func Get(ctx context.Context, db Querier, tableName string, options *FindOptions, dst interface{}) error {
-	sqlQuery, args := FindQuery(tableName, options)
+	sqlQuery, args := sqlquery.FindQuery(tableName, options)
 	return sqlscan.Get(ctx, db, dst, sqlQuery, args...)
 }
 
-// Select is a high-level function that calls FindAllQuery and scany sqlscan.Select function.
+// Select is a high-level function that calls sqlquery.FindAllQuery and scany sqlscan.Select function.
 func Select(ctx context.Context, db Querier, tableName string, options *FindAllOptions, dst interface{}) error {
-	sqlQuery, args := FindAllQuery(tableName, options)
+	sqlQuery, args := sqlquery.FindAllQuery(tableName, options)
 	return sqlscan.Select(ctx, db, dst, sqlQuery, args...)
 }
 
-// Insert is a high-level function that calls InsertQuery and db.ExecContext.
+// Insert is a high-level function that calls sqlquery.InsertQuery and db.ExecContext.
 func Insert(ctx context.Context, db Querier, flavor Flavor, tag, tableName string, structValue interface{}) error {
-	sqlQuery, args := InsertQuery(flavor, tag, tableName, structValue)
+	sqlQuery, args := sqlquery.InsertQuery(flavor, tag, tableName, structValue)
 	return sqlscan.Get(ctx, db, structValue, sqlQuery, args...)
 }
 
-// Update is a high-level function that calls UpdateQuery and db.ExecContext.
+// Update is a high-level function that calls sqlquery.pdateQuery and db.ExecContext.
 func Update(ctx context.Context, db Querier, flavor Flavor, tag, tableName string, id interface{}, structValue interface{}) error {
-	sqlQuery, args := UpdateQuery(flavor, tag, tableName, id, structValue)
+	sqlQuery, args := sqlquery.UpdateQuery(flavor, tag, tableName, id, structValue)
 	_, err := db.ExecContext(ctx, sqlQuery, args...)
 	return err
 }
 
-// Delete is a high-level function that calls DeleteQuery and db.ExecContext.
+// Delete is a high-level function that calls sqlquery.DeleteQuery and db.ExecContext.
 func Delete(ctx context.Context, db Querier, flavor Flavor, tableName string, id interface{}) error {
-	sqlQuery, args := DeleteQuery(flavor, tableName, id)
+	sqlQuery, args := sqlquery.DeleteQuery(flavor, tableName, id)
 	_, err := db.ExecContext(ctx, sqlQuery, args...)
 	return err
 }
